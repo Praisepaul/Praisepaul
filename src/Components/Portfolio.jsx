54e1cca64c800bb011fd112dcf2a514df19c3d7a
Praise Paul <praise20024christ@gmail.com>
2023-07-22 19:47:47 +0000

    Chnages to Portfolio Page

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -45,8 +45,8 @@ const projectList = [
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "My Personal Portfolio page developed using ReactJS and deployed to GitHub pages. Includes my experience and design abilities.",
+    url: "https://www.praisepaul.engineer",
   },
   {
     title: "GitHub Codespaces and github.dev",
